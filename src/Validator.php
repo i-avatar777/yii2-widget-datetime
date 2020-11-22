c347d3aea4b91d487d06b677cd6e729a926c83f8
ramha <dram1008@yandex.ru>
2020-11-22 16:43:01 +0300

    0.0.1

---
+++ src/Validator.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace iAvatar777\widgets\DateTime;
+
+
+
+class Validator extends \yii\validators\Validator
+{
+
+    public function validateAttribute($model, $attribute)
+    {
+
+    }
+
+    public function clientValidateAttribute($model, $attribute, $view)
+    {
+
+    }
+}
